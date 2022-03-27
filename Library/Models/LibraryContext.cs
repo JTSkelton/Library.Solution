1dddabe80638b3eda9fa172e609bc7436fd510b6
John Whitten <johnwhitten.studio@gmail.com>
2022-03-27 11:34:11 -0700

    add LibraryContext model content

--- Library/Models/LibraryContext.cs
+++ Library/Models/LibraryContext.cs
@@ -3,12 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Library.Models
 {
-  public class ToDoListContext : IdentityDbContext<ApplicationUser>
+  public class LibraryContext : IdentityDbContext<ApplicationUser>
   {
     public DbSet<Book> Books { get; set; }
     public DbSet<Librarian> Librarians { get; set; }
     public DbSet<Patron> Patrons { get; set; }
-    public DbSet<CategoryItem> CategoryItem { get; set; }
+    public DbSet<BookLibrarian> BookLibrarian { get; set; }
+    public DbSet<BookPatron> BookPatron { get; set; }
 
     public LibraryContext(DbContextOptions options) : base(options) { }
 
