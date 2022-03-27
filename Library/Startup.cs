04f7c41ad0f2631d28462c0a6fa1f4887244bf6e
John Whitten <johnwhitten.studio@gmail.com>
2022-03-27 11:35:32 -0700

    update Startup.cs for identity

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Library.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Library
 {
@@ -27,12 +28,19 @@ namespace Library
       services.AddEntityFrameworkMySql()
         .AddDbContext<LibraryContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<LibraryContext>()
+            .AddDefaultTokenProviders();
+   
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
+      app.UseAuthentication(); 
       app.UseRouting();
+      app.UseAuthorization();
 
       app.UseEndpoints(routes =>
       {
